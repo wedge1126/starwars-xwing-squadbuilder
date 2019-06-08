912187af8dba682c5e33dff5f8b98544c819183d
Jonathan Veilleux <wedge1126@gmail.com>
2019-06-08 01:45:52 -0700

    add scrolling to panels.

--- xwing-squadbuilder/src/ShipsPanel.js
+++ xwing-squadbuilder/src/ShipsPanel.js
@@ -6,7 +6,9 @@ export default class ShipsPanel extends React.Component {
         const { ships, onAddToSquad, selectedShips } = this.props;
         const pilotLists = ships.map((shipType, i) => <PilotList shiptype={shipType} selectedShips={selectedShips} onAddToSquad={onAddToSquad} key={i} />)
         return <div className="panel ships-panel">
-            {pilotLists}
+            <div className="ships-panel-pilot-list">
+                {pilotLists}
+            </div>
         </div>
     }
 }
